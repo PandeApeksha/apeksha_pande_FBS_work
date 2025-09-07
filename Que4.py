4fe54a9029d9694245b87a424bb883d3314ef8ee
PandeApeksha <154221653+PandeApeksha@users.noreply.github.com>
2025-09-07 10:11:43 +0530

    Delete Que4.py

--- Que4.py
+++
@@ -1,8 +0,0 @@
-interior_cost = int(input('Enter the cost of interior wall cost :'))
-area = int(input('Enter the area of wall :'))
-
-interior_area = 4 * area
-
-total_cost = interior_area * interior_cost
-
-print(f'total cost of painting is :{total_cost}')
