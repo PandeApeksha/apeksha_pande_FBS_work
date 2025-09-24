7b160b1dc084d7fa43b8e5308ded609005f59693
PandeApeksha <154221653+PandeApeksha@users.noreply.github.com>
2025-09-24 10:19:11 +0530

    Update armstrongNo.py

--- Core_Python/Assignments/Assignment4.py/armstrongNo.py
+++ Core_Python/Assignments/Assignment4.py/armstrongNo.py
@@ -6,20 +6,18 @@ for num in range(beg, end + 1):
     temp = num
     count = 0
 
-    while temp > 0:
+    while (temp > 0):
         temp = temp // 10
         count += 1
 
     temp = num
     sum_of_power = 0
 
-    while temp > 0:
+    while (temp > 0):
         digit = temp % 10
-        power = 1
-        for i in range(count):
-            power = power * digit
-        sum_of_power = sum_of_power + power
+        sum_of_power += digit ** count
         temp = temp // 10
 
     if sum_of_power == n:
         print(n)
+
