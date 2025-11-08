671337acf050e0fbb290049fe4915216f5c6b30b
PandeApeksha <154221653+PandeApeksha@users.noreply.github.com>
2025-11-08 21:09:28 +0530

    Delete Test1 directory

--- Test1/Question1.py
+++
@@ -1,9 +0,0 @@
-l = int(input('Enter the length : '))
-b = int(input('Enter the breadth :'))
-r = int(input('Enter the radius :'))
-
-area = l * b + (1/2 *3.14* (r**r))
-print(f'Area : {area}')
-
-perimeter =  2 * b + l + (3.14 * r) 
-print(f'perimeter : {perimeter}')
--- Test1/Question3.py
+++
@@ -1,4 +0,0 @@
-km = int(input('Enter Distance :'))
-meter =km * 1000
-centimeter = km * 100000
-print(f'meter : {meter}, centimeter : {centimeter}')
